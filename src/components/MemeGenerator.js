2b479a7a69188f359b6ca9c03c6433e013ca83a2
LaszloNagy85 <n.laszlo.uk@gmail.com>
2020-06-16 09:58:45 +0200

    add base files, skeleton structure

---
+++ src/components/MemeGenerator.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class MemeGenerator extends React.Component {
+
+    render() {
+        return(
+            <h3>
+                Meme Generator section
+            </h3>
+        )
+    }
+}
+
+export default MemeGenerator;
