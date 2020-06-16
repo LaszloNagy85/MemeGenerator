9117f0bd0a1624ba15651c3bf133c12cb099f3c6
LaszloNagy85 <n.laszlo.uk@gmail.com>
2020-06-16 10:06:19 +0200

    add header image and text

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,9 @@ import React from 'react';
 function Header() {
     return(
         <header>
-            This is the header
+            <img src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png"
+                alt="" />
+            <p>Meme Generator</p>
         </header>
     )
 }
